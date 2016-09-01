3074302e0481f845338e195e98198b6ccf5c26d8
Ross Moroney <code.ph0y@googlemail.com>
2016-09-01 03:55:17 +0100

    Add: Base Interface
    - Refactor: Location of Bower files bower.json, .bowerrc
    - Add: font-awesome
    - Add: Bootstap Nav Bar
    - Add: create todo template for future development

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DimporterBundle:Default:index.html.twig');
+        return $this->render('DimporterBundle:default:index.html.twig');
     }
 }
