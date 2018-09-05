82173fce833b56ad4a22231c1b79b5106628e4ac
Ross Moroney <code.ph0y@googlemail.com>
2018-09-05 23:25:49 +0100

    Added: Textarea field for the input template, Fixed: padding

--- mvc/View/home/index.html.php
+++ mvc/View/home/index.html.php
@@ -1,1 +1,8 @@
-<p>This is working<p>
+<div class="row">
+    <div class="col-12">
+        <div class="form-group">
+            <label for="inputTemplate">Input Template</label>
+            <textarea class="form-control" id="inputTemplate" rows="3"></textarea>
+        </div>
+    </div>
+</div>
