ad90aac7f4411da504e68c942781069d24dd3cbf
Ross Moroney <code.ph0y@googlemail.com>
2018-09-05 10:17:58 +0100

    Refactor: Rename Project folders, Add: Placeholder file in assets folder

---
+++ mvc/Base/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mvc\Base;
+
+class BaseController {
+
+    /**
+     * Render a View File
+     *
+     * @param  string $view   [description]
+     * @param  mixed $params [description]
+     * @return [type]         [description]
+     */
+    public function render($view, $params)
+    {
+        require_once '';
+    }
+}
