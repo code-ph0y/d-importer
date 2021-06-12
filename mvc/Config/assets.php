1ab2422b6db4118f853f0e8af8dda193bdb71389
codeph0y <code.ph0y@googlemail.com>
2021-06-12 11:03:51 +0100

    Feature #4 - Create templates for pages

--- mvc/Config/assets.php
+++ mvc/Config/assets.php
@@ -4,11 +4,16 @@
 $config['assets']['js'] = [
     'https://code.jquery.com/jquery-3.6.0.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/split.js/1.6.0/split.min.js',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js',
+    'https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js',
+    'assets/main.js'
 ];
 
 $config['assets']['css'] = [
+    'assets/css/font-awesome.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css',
+    'https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css',
+    'assets/main.css'
 ];
 
