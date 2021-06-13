fc8bb558201f2c506e1f218bac44e6c7e501a6c7
codeph0y <code.ph0y@googlemail.com>
2021-06-13 20:02:55 +0100

    Add datasource base fields

---
+++ mvc/Config/datasources/fields.php
@@ -0,0 +1,19 @@
+<?php
+
+$fields['datasources']['fields'] = [
+    'fieldName' => 'id',
+    'type' => 'hash',
+    'len' => '255'
+];
+
+$fields['datasources']['fields'] = [
+    'fieldName' => 'name',
+    'type' => 'varchar',
+    'len' => '255'
+];
+
+$fields['datasources']['fields'] = [
+    'fieldName' => 'type',
+    'type' => 'varchar',
+    'len' => '255'
+];
---
+++ mvc/Config/datasources/types/mysql.php
@@ -0,0 +1,4 @@
+<?php
+
+$fields['datasources']['fields'] = [
+];
