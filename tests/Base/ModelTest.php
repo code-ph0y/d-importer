41a7fd536191fa1cf08c7fc9860657f78d597a24
codeph0y <code.ph0y@googlemail.com>
2021-06-26 10:44:30 +0100

    Add snub tests for Base

---
+++ tests/Base/ModelTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class ModelTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
