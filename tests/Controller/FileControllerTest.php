39a0369a99583606de297047f0aed09d35d8831d
codeph0y <code.ph0y@googlemail.com>
2021-06-26 10:40:49 +0100

    Add snub tests for controllers

---
+++ tests/Controller/FileControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class FileControllerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
